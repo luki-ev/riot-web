16b365e05cf6427cad0def48cfa60e089df53dba
Christoph Settgast <csett86@web.de>
2019-11-16 22:20:32 +0100

    branding: translate step 1 of mobile guide

--- src/vector/mobile_guide/index.js
+++ src/vector/mobile_guide/index.js
@@ -95,7 +95,7 @@ async function initPage() {
         document.getElementById('configure_riot_button').href =
             "https://riot.im/config/config?hs_url=" + encodeURIComponent(hsUrl) +
             "&is_url=" + encodeURIComponent(isUrl);
-        document.getElementById('step1_heading').innerHTML= '1: Install the app';
+        document.getElementById('step1_heading').innerHTML= '1: App installieren';
         document.getElementById('step2_container').style.display = 'block';
         document.getElementById('hs_url').innerText = hsUrl;
 
